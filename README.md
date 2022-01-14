f981c2bede480562a632bec8e60b84f75a946aa7
root <root@LAPTOP-P22ONHTH.localdomain>
2022-01-13 19:23:24 -0600

    Agrege funcionalidades al circulo y al cuadrado

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # web-matematicas-basicas
+
+## Taller |1| : Figuraas geometricas. 
+
+- Primer paso:  definir las formulas
+- Segundo paso: implementar las formulas en js
+- Tercer paso:  Crear funciones
+- Cuarto paso:  integrar js a html.
+
+
---
+++ js/figuras.js
@@ -0,0 +1,114 @@
+// Código del cuadrado
+console.group("Cuadrados");
+// const ladoCuadrado = 5;
+// console.log("Los lados del cuadrado miden: " + ladoCuadrado + "cm");
+
+function perimetroCuadrado(lado) {
+  return lado * 4;
+}
+// console.log("El perímetro del cuadrado es: " + perimetroCuadrado + "cm");
+
+function areaCuadrado(lado) {
+  return lado * lado;
+}
+// console.log("El área del cuadrado es: " + areaCuadrado + "cmˆ2");
+console.groupEnd();
+
+// Código del triángulo
+console.group("Triángulos");
+
+
+// const alturaTriangulo = 5.5;
+// console.log("La altura del triángulo es de: " + alturaTriangulo + "cm");
+
+function perimetroTriangulo(lado1, lado2, base) {
+  return lado1 + lado2 + base;
+}
+// console.log("El perímetro del triángulo es: " + perimetroTriangulo + "cm");
+
+function areaTriangulo(base, altura) {
+  return (base * altura) / 2;
+}
+console.log("El área del triángulo es: " + areaTriangulo + "cmˆ2");
+
+console.groupEnd();
+
+// Código del círculo
+console.group("Círculos");
+
+// Radio
+// const radioCirculo = 4;
+// console.log("El radio del círculo es: " + radioCirculo + "cm");
+
+// Diámetro
+function diametroCirculo(radio) {
+  return radio * 2;
+}
+
+// PI
+const PI = Math.PI;
+console.log("PI es: " + PI);
+
+// Circunferencia
+function perimetroCirculo(radio) {
+  const diametro = diametroCirculo(radio);
+  return diametro * PI;
+}
+
+// Área
+function areaCirculo(radio) {
+  return (radio * radio) * PI;
+}
+
+console.groupEnd();
+
+
+
+
+
+
+
+
+
+
+
+// Aquí interactuamos con el HTML
+function calcularPerimetroCuadrado() {
+  const input = document.getElementById("InputCuadrado");
+  const value = input.value;
+
+  const perimetro = perimetroCuadrado(value);
+  alert("El perimetro del cuadrado es: "+perimetro);
+
+}
+
+function calcularAreaCuadrado() {
+  const input = document.getElementById("InputCuadrado");
+  const value = input.value;
+
+  const area = areaCuadrado(value);
+  alert("El area del cuacdrado es: "+area);
+
+}
+
+// Aqui interactuamos con el HTML del circulo
+function calcularPrimeroCirculo() {
+   const input = document.getElementById("InputCirculo");
+  const value = input.value;
+
+  const perimetro = perimetroCirculo(value);
+  alert("El perimetro del circulo es: "+perimetro);
+}
+
+function calcularAreaCirculo() {
+   const input = document.getElementById("InputCirculo");
+   const value = input.value;
+   const area = areaCirculo(value);
+    alert("El area del circulo es: "+area);
+}
+function calcularDiametroCirculo(){
+  const input = document.getElementById("InputCirculo");
+  const value = input.value;
+  const perimetro = diametroCirculo(value);
+  alert("El perimetro del circulo es: "+perimetro);
+}
